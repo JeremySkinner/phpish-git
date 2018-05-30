1d16d95fe01a28bd855cc4f7bb09a8aeed1a1526
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-05-30 23:29:53 +0100

    Rename utils -> text utils

--- git-prompt.php
+++ git-prompt.php
@@ -3,7 +3,7 @@ require 'src/settings.php';
 require 'src/logger.php';
 require 'src/git.php';
 require 'src/prompt.php';
-require 'src/utils.php';
+require 'src/text-utils.php';
 
 $settings = GitSettings::create();
 $prompt = Prompt::create($settings);

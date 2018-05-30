c9a5abc1ce3bb9ff012b3a7d9d5e62f5042c9e72
Jeremy Skinner <jeremy@jeremyskinner.co.uk>
2018-05-31 00:05:58 +0100

    Update variable

--- git-prompt.sh
+++ git-prompt.sh
@@ -1,5 +1,5 @@
 git-prompt () {
     local my_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-    local prompt=`php "${my_dir}/phpish-git/git-prompt.php"`
+    local prompt=`php "${my_dir}/git-prompt.php"`
     export PS1=$prompt
 }
